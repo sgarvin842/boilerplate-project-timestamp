bd5ce41e8d47453a6949e76d28c909ab224871f5
Scott Garvin <sgarvin842@outlook.com>
2024-05-01 05:38:16 +0000

    updated the parameters for api request

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/:date", function (req, res) {
+app.get("/api/:date?", function (req, res) {
   let date = new Date(req.params.date);
   if(isNaN(date)){
     date = new Date(parseInt(req.params.date))
