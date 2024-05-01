eb5951b3718e8ee3e2bade9d857fe4457b9c38e6
Scott Garvin <sgarvin842@outlook.com>
2024-05-01 04:12:53 +0000

    Updated endpoint to match the expected request and give a generic request back

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function (req, res) {
+  res.json({greeting: req.params.date});
 });
 
 
